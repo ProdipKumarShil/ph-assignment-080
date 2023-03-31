a7631d6f3b82e2a4e8a57bae4d8f1c9c4a76bce2
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-03-31 10:15:07 +0600

    added header

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './Header/Header'
+import BlogAndBookmark from './components/BlogAndBookmark/BlogAndBookmark'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header></Header>
+      <BlogAndBookmark></BlogAndBookmark>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/BlogAndBookmark/BlogAndBookmark.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Blog from '../Blog/Blog';
+import Bookmark from '../Bookmark/Bookmark';
+
+const BlogAndBookmark = () => {
+    return (
+        <div>
+            <Blog></Blog>
+            <Bookmark></Bookmark>
+        </div>
+    );
+};
+
+export default BlogAndBookmark;
